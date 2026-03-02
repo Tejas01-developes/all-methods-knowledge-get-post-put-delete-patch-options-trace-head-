107ae95bdb5b80a27436174f277d0c832da922fc
Tejas01-developes <tlimbad7@gmail.com>
2026-03-03 01:43:04 +0530

    createpool added

--- dbconnection/dbconnection.js
+++ dbconnection/dbconnection.js
@@ -2,11 +2,14 @@ import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const db=mysql.createConnection({
+export const db=mysql.createPool({
+    connectionLimit:10,
     host:process.env.DB_HOST,
-    port:process.env.DB_PORT,
-    database:process.env.DB_NAME,
-    password:process.env.DB_PASS,
-    user:process.env.DB_ROOT
-    
+        port:process.env.DB_PORT,
+        database:process.env.DB_NAME,
+        password:process.env.DB_PASS,
+        user:process.env.DB_ROOT
+        
 })
+
+
