c5e875b2b0fcae7096059e77aed074914908174f
Tejas01-developes <tlimbad7@gmail.com>
2026-03-03 18:47:50 +0530

    made service file

--- apis/apis.js
+++ apis/apis.js
@@ -2,113 +2,94 @@ import { db } from "../dbconnection/dbconnection.js";
 import bcrypt from 'bcrypt';
 import { token } from "../tokens/token.js";
 import jwt from 'jsonwebtoken';
+import { deletequery, findquery, getquery, insertquery, selectquery, updatequery } from "../query_file/queries.js";
+
+
 export const insertuser=async(req,resp)=>{
 const {id,name,password}=req.body;
 if(!id || !name || !password){
     return resp.status(400).json({success:false,message:"data not recived"})
 }
 const hash=await bcrypt.hash(password,10);
-db.query(
-    'insert into users1 (id,name,password) values (?,?,?)',
-    [id,name,hash],
-    (err)=>{
-        if(err){
-            return resp.status(400).json({success:false,message:"db insertion failed"})
-        }
-        return resp.status(200).json({success:true,message:"inserted succesfully"})
-    }
-)
+try{
+
+
+const insert=await insertquery({id,name,password:hash})
+return resp.status(200).json({success:true,message:"db insert success"})
+
+}catch(err){
+console.log("error",err)
+}
 
 }
 
 
-export const getdata=(req,resp)=>{
-    db.query(
-        'select * from users1',
-        (err,res)=>{
-            if(err){
-                return resp.status(400).json({success:false,message:"db fetch failed"})
-            }
-            return resp.status(200).json({success:true,res})
-        }
-    )
+export const getdata=async(req,resp)=>{
+    try{
+   
+    const getdata=await selectquery()
+    return resp.status(200).json({success:true,"result":getdata})
+}catch(err){
+    console.log("error",err)
+}
 }
 
 
 export const update=async(req,resp)=>{
     const{name,password}=req.body;
-    // const{password}=req.body;
-   
-    db.query(
-        'select name,password from users1 where name=?',
-        [name],
-      async  (err,res)=>{
-            if(err){
-            return resp.status(400).json({success:false,message:"error seeing user with this name in the db"})
-        }
-        if(res.length === 0){
-            return resp.status(400).json({success:false,message:"no user with this name in the db"}) 
-        }
-        
+    if(!name || !password){
+        return resp.status(400).json({success:false,message:"fields are not present"})
+    }
+   try{
+  const pass=await getquery({name,password})
+  
 
-        const compare=await bcrypt.compare(password,res[0].password);
-        console.log(compare);
+        const compare=await bcrypt.compare(password,pass);
+       
         if(compare){
             return resp.status(400).json({success:false,message:"same password as previous"})
         }
         const hash=await bcrypt.hash(password,10);
-        db.query(
-            
-            'update users1 set password=? where name=?',
-            [hash,name],
-            (err)=>{
-                if(err){
-                    return resp.status(400).json({success:false,message:"update failed from db"})
-                }
-                return resp.status(200).json({success:true,message:"update succesfully"})
-            }
-
-        )
-    }
-   
-
-        
-    )
+     
+      const updation=await updatequery({password:hash,name})
+      return resp.status(200).json({success:true,message:"password updated succesfully"})
+    }catch(err){
+        console.log("error",err)
+    }    
 }
 
 
-export const deleteusers=(req,resp)=>{
+
+
+export const deleteusers=async(req,resp)=>{
 const{name}=req.body;
-db.query(
-    'select name from users1 where name=?',
-    [name],
-    (err,res)=>{
-        if(err){
-            return resp.status(400).json({success:false,message:"db delete error"})
-        }
-        if(res.length === 0){
-            return resp.status(400).json({success:false,message:"no user with this name"})
-        }
-        db.query(
-            'delete from users1 where name=?',
-        [name],
-        (err)=>{
-            if(err){
-                return resp.status(400).json({success:false,message:"error db deletation"})
-            }
-            return resp.status(200).json({success:true,message:"delete succesfully done"})
-        }
-        )
+if(!name){
+    return resp.status(400).json({success:false,message:"name field is not present"})
+}
+try{
+
+const findname=await findquery({name})
+
+        const deleteuser=await deletequery({name})
+        return resp.json({deleteuser})
+        return resp.status(200).json({success:true,message:"deletation success"})
+    }catch(err){
+        console.log("error",err)
     }
-)
+    
 
 }
+
 
 
 
 export const login=async(req,resp)=>{
     const{name,password}=req.body;
 
+    if(!name || !password){
+        return resp.status(400).json({success:false,message:"fields are not present"})
+    }
+try{
     db.query(
         'select name,password from users1 where name=?',
         [name],
@@ -135,4 +116,7 @@ export const login=async(req,resp)=>{
             return resp.status(200).json({success:true,message:"login success","token":decode})
         }
     )
+}catch(err){
+    console.log("error",err)
+}
 }
---
+++ query_file/queries.js
@@ -0,0 +1,106 @@
+import { db } from "../dbconnection/dbconnection.js"
+
+export const insertquery=(data)=>{
+    return new Promise((resolve,reject)=>{
+        db.query(
+            'insert into users1 (id,name,password) values (?,?,?)',
+            [data.id,data.name,data.password],
+            (err)=>{
+                if(err){
+                    return reject("error")
+                }
+                return resolve("success")
+            }
+        )
+    })
+   
+}
+
+
+export const selectquery=(data)=>{
+    return new Promise((resolve,reject)=>{
+        db.query(
+            'select * from users1 ',
+            (err,res)=>{
+                if(err){
+                    return reject("error")
+                }
+                return resolve(res)
+            }
+        )
+    })
+
+}
+
+
+export const getquery=(data)=>{
+    return new Promise((resolve,reject)=>{
+        db.query(
+            'select name,password from users1 where name= ? ',
+            [data.name],
+            (err,res)=>{
+                if(err){
+                    return reject(err)
+                }
+                if(res.length === 0){
+                    return reject(err)
+                }
+                resolve(res[0].password);
+            }
+        )
+    })
+   
+    }
+
+    export const updatequery=(data)=>{
+       return new Promise((resolve,reject)=>{
+        db.query(
+            'update users1 set password= ? where name= ?',
+            [data.password,data.name],
+            (err)=>{
+                if(err){
+                    return reject("error",err)
+                }
+                return resolve("success")
+            }
+        )
+       })
+       
+       
+    }
+
+
+    export const findquery=(data)=>{
+        return new Promise((resolve,reject)=>{
+            db.query(
+                "select name from users1 where name=?",
+                [data.name],
+                (err,res)=>{
+                    if(err){
+                        return reject("finding error")
+                    }
+                    if(res.length === 0){
+                        return reject("no user")
+                    }
+                    resolve("user is present")
+                }
+            )
+        })
+        
+    }
+
+
+    export const deletequery=(data)=>{
+        return new Promise((resolve,reject)=>{
+            db.query(
+                'delete  from users1 where name=?',
+                [data.name],
+                (err)=>{
+                    if(err){
+                        return reject("deletation error")
+                    }
+                    return resolve("success")
+                }
+            )
+        })
+    }
